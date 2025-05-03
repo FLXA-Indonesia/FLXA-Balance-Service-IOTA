d6170237ea4c8dedf3d9ccf2e9e1565c6d994feb
PetrusAriaa <petrusariacr25@gmail.com>
2025-05-03 13:24:44 +0700

    Revert "feat: create token balance on balance creation"
    This reverts commit 06cba147ed7dc941b02af3138836df8680856b8e.

--- src/controllers/balance.js
+++ src/controllers/balance.js
@@ -12,20 +12,11 @@ exports.createBalance = (req, res) => {
   const values = [userId, 0]
 
   db.query(sql, values)
-    .then((balanceResult) => {
-      db.query(`INSERT INTO "Token_balance" (user_id) VALUES ($1) RETURNING *`, [userId])
-        .then((tokenBalanceResult) => {
-          res.status(201).json({
-            message: 'Balance created successfully',
-            data: {
-              balance: balanceResult.rows[0],
-              tokenBalance: tokenBalanceResult.rows[0]
-            }
-          })
-        })
-        .catch((error) => {
-          console.error('Error creating token balance:', error)
-        })
+    .then((result) => {
+      res.status(201).json({
+        message: 'Balance created successfully',
+        data: result.rows[0]
+      })
     })
     .catch((error) => {
       console.error('Error creating balance:', error)
